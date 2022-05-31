5ef9142322669f6cc409c88e451164057d2df36d
Cory Metcalf <corymetcalf@Corys-Macbook-Pro.local>
2022-05-31 13:39:00 -0400

    Created Update/Edit and Delete routes for Night Routine CRUD.

--- controllers/night.js
+++ controllers/night.js
@@ -19,12 +19,45 @@ router.get('/new', (req, res) => {
     res.render('night/new.ejs');
 });
 
+// Delete
+router.delete('/:id', (req, res) => {
+    Night.findByIdAndRemove(req.params.id, (err, data) => {
+        res.redirect("/night");
+    });
+});
+
+// Update
+router.put('/:id', (req, res) => {
+    Night.findByIdAndUpdate(req.params.id, req.body, () => {
+        res.redirect('/night')
+    });
+});
+
 // Create
 router.post('/', (req, res) => {
     Night.create(req.body, (err, createdNight) =>{
         res.redirect('/night');
     });
 });
+
+// Edit
+router.get('/:id/edit', (req, res) => {
+    Night.findById(req.params.id, (err, foundNight) => {
+        res.render('night/edit.ejs', {
+            night: foundNight
+        });
+    });
+});
+
+// Show
+router.get('/:id', (req, res) => {
+    Night.findById(req.params.id, (err, foundNight) => {
+        res.render('night/show.ejs', {
+            night: foundNight
+        });
+    });
+});
+
 
 // Export
 module.exports = router;
