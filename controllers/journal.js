7a3ecc79548fcbaeea5d3a75e62cca6d3fe58da4
Cory Metcalf <corymetcalf@Corys-Macbook-Pro.local>
2022-06-01 11:23:50 -0400

    Full CRUD for the journal

--- controllers/journal.js
+++ controllers/journal.js
@@ -19,10 +19,33 @@ router.get('/new', (req, res) => {
     res.render('journal/new.ejs');
 });
 
+// Delete
+router.delete('/:id', (req, res) => {
+    Journal.findByIdAndRemove(req.params.id, () => {
+        res.redirect('/journal');
+    });
+});
+
+// Update
+router.put('/:id', (req, res) => {
+    Journal.findByIdAndUpdate(req.params.id, req.body, () => {
+        res.redirect('/journal')
+    });
+});
+
 // Create
 router.post('/', (req, res) => {
     Journal.create(req.body, (err, createdJournal) => {
         res.redirect('/journal');
+    });
+});
+
+// Edit
+router.get('/:id/edit', (req, res) => {
+    Journal.findById(req.params.id, (err, foundJournal) => {
+        res.render('journal/edit.ejs', {
+            journal: foundJournal
+        });
     });
 });
 
